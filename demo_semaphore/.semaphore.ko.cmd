1890d4d87b1e18de67dc24b67286441bff2a6245
rachana1998 <rachana2602@gmail.com>
2020-11-28 11:47:46 +0530

    demo for semaphore in driver

---
+++ demo_semaphore/.semaphore.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/rachana/char_drivers/demo_semaphore/semaphore.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000  --build-id  -T ./scripts/module-common.lds -o /home/rachana/char_drivers/demo_semaphore/semaphore.ko /home/rachana/char_drivers/demo_semaphore/semaphore.o /home/rachana/char_drivers/demo_semaphore/semaphore.mod.o;  true
---
+++ demo_semaphore/.semaphore.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/rachana/char_drivers/demo_semaphore/semaphore.mod := { echo  /home/rachana/char_drivers/demo_semaphore/semaphore.o; echo; } > /home/rachana/char_drivers/demo_semaphore/semaphore.mod
